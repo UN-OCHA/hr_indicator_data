fa0b307c16f0ba61df4a768c8ff391710c99dfbd
Lars Kleiner <larskleiner@googlemail.com>
2013-02-13 13:15:48 +0100

    Indicator Value Items indicators table display (https://trello.com/c/zKo9AOxh)

---
+++ templates/indicator-data-node-form.tpl.php
@@ -0,0 +1,6 @@
+<?php print drupal_render_children($form['field_ind_date']); ?>
+<?php print drupal_render_children($form['field_cluster']); ?>
+<?php print drupal_render_children($form['field_ind_def']); ?>
+<?php print drupal_render_children($form['field_locations']); ?>
+<?php print $indicators; ?>
+<?php print drupal_render_children($form); ?>
